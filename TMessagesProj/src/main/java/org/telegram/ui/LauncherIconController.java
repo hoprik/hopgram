f5b6c33254f7374f23a07b4415d0fee59b53b080
Hoprik V. Morozov <hoprik@hoprik.ru>
2024-12-10 21:45:48 +0300

    теперь в настройках лого из hopgram

--- TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
@@ -34,12 +34,12 @@ public class LauncherIconController {
     }
 
     public enum LauncherIcon {
-        DEFAULT("DefaultIcon", R.drawable.icon_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconDefault),
-        VINTAGE("VintageIcon", R.drawable.icon_6_background_sa, R.mipmap.icon_6_foreground_sa, R.string.AppIconVintage),
-        AQUA("AquaIcon", R.drawable.icon_4_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconAqua),
-        PREMIUM("PremiumIcon", R.drawable.icon_3_background_sa, R.mipmap.icon_3_foreground_sa, R.string.AppIconPremium, true),
-        TURBO("TurboIcon", R.drawable.icon_5_background_sa, R.mipmap.icon_5_foreground_sa, R.string.AppIconTurbo, true),
-        NOX("NoxIcon", R.mipmap.icon_2_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconNox, true);
+        DEFAULT("DefaultIcon", R.drawable.icon_background_sa, R.mipmap.ic_hopgram_round, R.string.AppIconDefault),
+        VINTAGE("VintageIcon", R.drawable.icon_6_background_sa, R.mipmap.ic_hopgram_round, R.string.AppIconVintage),
+        AQUA("AquaIcon", R.drawable.icon_4_background_sa, R.mipmap.ic_hopgram_round, R.string.AppIconAqua),
+        PREMIUM("PremiumIcon", R.drawable.icon_3_background_sa, R.mipmap.ic_hopgram_round, R.string.AppIconPremium, true),
+        TURBO("TurboIcon", R.drawable.icon_5_background_sa, R.mipmap.ic_hopgram_round, R.string.AppIconTurbo, true),
+        NOX("NoxIcon", R.mipmap.icon_2_background_sa, R.mipmap.ic_hopgram_round, R.string.AppIconNox, true);
 
         public final String key;
         public final int background;
